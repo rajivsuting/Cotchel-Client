0d91b4d32ea8dd02fe65478e07c0ded1089f9118
rajivsuting <rajivsuting16@gmail.com>
2025-07-26 16:31:45 +0530

    update api

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-// "https://starfish-app-6q6ot.ondigitalocean.app/api";
+  import.meta.env.VITE_API_URL ||
+  "https://starfish-app-6q6ot.ondigitalocean.app/api";
 // "http://localhost:5000/api";
 
 export const API = {
